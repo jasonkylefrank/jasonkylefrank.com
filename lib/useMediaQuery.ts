59c07d724351d3b9b9e131ee9c34a2380a7fd2e7
Jason K Frank <jasonkylefrank@users.noreply.github.com>
2023-02-10 23:26:59 -0500

    Fixed conditional state hook

--- lib/useMediaQuery.ts
+++ lib/useMediaQuery.ts
@@ -2,28 +2,25 @@ import { useState, useEffect } from "react";
 
 export function useMediaQuery(query: string) {
   const isOnClient = typeof window !== "undefined";
+  const initialVal = isOnClient ? window.matchMedia(query).matches : false;
 
-  if (isOnClient) {
-    const [matches, setMatches] = useState(window.matchMedia(query).matches);
+  const [matches, setMatches] = useState(initialVal);
 
-    useEffect(() => {
-      const media = window.matchMedia(query);
+  useEffect(() => {
+    const media = window.matchMedia(query);
 
-      if (media.matches !== matches) {
-        setMatches(media.matches);
-      }
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
 
-      const listener = () => {
-        setMatches(media.matches);
-      };
+    const listener = () => {
+      setMatches(media.matches);
+    };
 
-      media.addEventListener("change", listener);
+    media.addEventListener("change", listener);
 
-      return () => media.removeEventListener("change", listener);
-    }, [matches, query]);
+    return () => media.removeEventListener("change", listener);
+  }, [matches, query]);
 
-    return matches;
-  } else {
-    return false;
-  }
+  return matches;
 }
