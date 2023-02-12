847ac646c607ffbe808c1f5ee82920a5f05cc7a0
Jason K Frank <jasonkylefrank@users.noreply.github.com>
2023-02-12 18:10:25 -0500

    Twitch page: styling; GitHub link

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,13 +19,26 @@ module.exports = {
       maxWidth: {
         "8xl": "88rem", // 1408px with a 16px base
       },
-      // typography: {
-      //   DEFAULT: {
-      //     css: {
-      //       h3: "margin-top: 2em", // default is 1.6666em
-      //     },
-      //   },
-      // },
+      typography: {
+        // This ends up being used for the smallest breakpoint
+        DEFAULT: {
+          css: {
+            h3: {
+              marginTop: "2em", // default is 1.6em (~32px) at the smaller breakpoints
+            },
+          },
+        },
+        // We have to specify styles for this breakpoint even if they are the same as we've stated
+        //  on smaller breakpoints because Tailwind's media-query-based class would have have higher
+        //  specificity than our 'default' styles for this size screen.
+        lg: {
+          css: {
+            h3: {
+              marginTop: "2em", // default is 1.6666em (40px) at this breakpoint
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
